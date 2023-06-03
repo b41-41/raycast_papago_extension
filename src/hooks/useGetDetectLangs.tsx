fb5a7d7978d631b89586d499005a158ea0fbc4bd
DABIN JEONG <41@b41.kr>
2023-06-04 02:28:31 +0900

    fix: delete prop required

--- src/hooks/useGetDetectLangs.tsx
+++ src/hooks/useGetDetectLangs.tsx
@@ -3,9 +3,7 @@ import { apiInstance } from "../api/axios";
 import { Query } from "../types";
 import { Toast, showToast } from "@raycast/api";
 
-type Props = {};
-
-const useGetDetectLangs = (props: Props) => {
+const useGetDetectLangs = () => {
   const getDetectLangs = async (query: Query) => {
     try {
       apiInstance.post("/detectLangs", qs.stringify({ query: query })).then((res) => {
