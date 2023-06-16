f4da29889cf4ae6f4019e10be0fad917f7da9c9b
DABIN JEONG <41@b41.kr>
2023-06-17 00:06:35 +0900

    feat: add target lang type

--- src/types.ts
+++ src/types.ts
@@ -20,3 +20,5 @@ export interface N2MTResponse {
 }
 
 export type SearchText = string;
+
+export type TargetLang = "ko" | "en" | "ja" | "zh-CN" | "zh-TW" | "vi" | "id" | "th" | "de" | "ru" | "es" | "it" | "fr";
